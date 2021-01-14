1d16f3debeb9ab931e93a30e1f738754923ac5cd
nurzzzone <nurzzzone@gmail.com>
2021-01-15 01:35:36 +0600

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 6000,
+    mouseDrag: false,
+    smartSpeed: 1200,
+    center: true,
+    dots: false,
+  });
+});
