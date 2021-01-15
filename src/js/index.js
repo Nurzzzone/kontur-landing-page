2b3d2705bca07d73fee68e2c1049a10ecef670a6
nurzzzone <nurzzzone@gmail.com>
2021-01-15 23:11:36 +0600

    Initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
     loop: false,
     items: 1,
-    autoplay: false,
+    autoplay: true,
     autoplayTimeout: 6000,
     mouseDrag: false,
     smartSpeed: 1200,
@@ -11,10 +11,58 @@ $(document).ready(function(){
   });
 });
 
-const burger = document.querySelector('.burger')
-const mobileMenu = document.querySelector('.header__mobile')
+const burger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('.header__mobile');
+const header = document.querySelector('#header');
+let mobileMenuOpen = false;
+let searching = false;
+
+window.addEventListener('scroll', function(){
+  if (window.scrollY > 0) {
+    header.classList.add('header--fixed');
+  } else {
+    header.classList.remove('header--fixed')
+  }
+})
 
 burger.addEventListener('click', function() {
+
   mobileMenu.classList.toggle('header__mobile--active');
-  console.log('hello')
+
+  document.body.style.paddingRight = `${getScrollBarWidth()}px`;
+  document.documentElement.style.overflow = "hidden";
+
 })
+
+document.querySelector('body').addEventListener('click', function(e) {
+
+    if(!e.target === mobileMenu && mobileMenu.children) {
+      console.log('hello')
+    }
+}) 
+
+
+
+
+if (mobileMenuOpen === true) {
+  disableScroll();
+} else {
+  window.onscroll = function() {};
+}
+
+function getScrollBarWidth() {
+  const item = document.createElement('div');
+
+  item.style.position = "absolute";
+  item.style.top = "-99999px";
+  item.style.width = "50px";
+  item.style.height = "50px";
+  item.style.overflow = "scroll";
+  item.style.visibility = "hidden";
+
+  document.body.appendChild(item);
+  const scrollBarWidth = item.offsetWidth - item.clientWidth;
+  document.body.removeChild(item);
+
+  return scrollBarWidth;
+}
