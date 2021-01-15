9b443d02a7d1a371c6a6bab2c71dd9244a88ce00
nurzzzone <nurzzzone@gmail.com>
2021-01-15 13:49:48 +0600

    Initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
-    loop: true,
+    loop: false,
     items: 1,
-    autoplay: true,
+    autoplay: false,
     autoplayTimeout: 6000,
     mouseDrag: false,
     smartSpeed: 1200,
@@ -10,3 +10,11 @@ $(document).ready(function(){
     dots: false,
   });
 });
+
+const burger = document.querySelector('.burger')
+const mobileMenu = document.querySelector('.header__mobile')
+
+burger.addEventListener('click', function() {
+  mobileMenu.classList.toggle('header__mobile--active');
+  console.log('hello')
+})
