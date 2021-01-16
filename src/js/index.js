9dbaf3ae2afced0603e787189764f337153e0d14
nurzzzone <nurzzzone@gmail.com>
2021-01-17 02:37:38 +0600

    Initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
-    loop: false,
+    loop: true,
     items: 1,
     autoplay: true,
     autoplayTimeout: 6000,
@@ -14,9 +14,10 @@ $(document).ready(function(){
 const burger = document.querySelector('.burger');
 const mobileMenu = document.querySelector('.header__mobile');
 const header = document.querySelector('#header');
-let mobileMenuOpen = false;
+const searchInput = document.querySelector('#search-input')
 let searching = false;
 
+// Add fixed header on scroll
 window.addEventListener('scroll', function(){
   if (window.scrollY > 0) {
     header.classList.add('header--fixed');
@@ -25,44 +26,23 @@ window.addEventListener('scroll', function(){
   }
 })
 
+// Open Mobile Menu on click
 burger.addEventListener('click', function() {
-
   mobileMenu.classList.toggle('header__mobile--active');
-
-  document.body.style.paddingRight = `${getScrollBarWidth()}px`;
-  document.documentElement.style.overflow = "hidden";
-
 })
 
-document.querySelector('body').addEventListener('click', function(e) {
+// Close Mobile Menu on click outside
+document.addEventListener('click', function(e) {
+  if (e.target.closest('.burger') || e.target.closest('.header__mobile')) {
+    return
+  } else {
+    mobileMenu.classList.remove('header__mobile--active');
+  }
+})
 
-    if(!e.target === mobileMenu && mobileMenu.children) {
-      console.log('hello')
-    }
-}) 
+// searchInput.addEventListener('click', function() {
+//   document.querySelector('.header__right').style.display = "none";
+//   header.style.backgroundColor = "black";
+// })
 
 
-
-
-if (mobileMenuOpen === true) {
-  disableScroll();
-} else {
-  window.onscroll = function() {};
-}
-
-function getScrollBarWidth() {
-  const item = document.createElement('div');
-
-  item.style.position = "absolute";
-  item.style.top = "-99999px";
-  item.style.width = "50px";
-  item.style.height = "50px";
-  item.style.overflow = "scroll";
-  item.style.visibility = "hidden";
-
-  document.body.appendChild(item);
-  const scrollBarWidth = item.offsetWidth - item.clientWidth;
-  document.body.removeChild(item);
-
-  return scrollBarWidth;
-}
