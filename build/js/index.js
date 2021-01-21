19b46bd2e2127e879d4ff4023c2901518e07c330
nurzzzone <nurzzzone@gmail.com>
2021-01-21 13:03:58 +0600

    Initial commit

--- build/js/index.js
+++ build/js/index.js
@@ -1,3 +1,4 @@
+// Owl-Carousel Home Section
 $(document).ready(function(){
   $(".home__items").owlCarousel({
     loop: true,
@@ -18,36 +19,57 @@ const header = document.querySelector('#header');
 const searchInput = document.querySelector('#search-input');
 const orderBtn = document.querySelector('#order-btn');
 const logo = document.querySelector('.header__logo');
+const scrollToTop = document.querySelector('.scroll-top');
 
 
-// Add fixed header on scroll
 window.addEventListener('scroll', function(){
-  if (window.scrollY > 0) {
+  const scroll = window.pageYOffset;
+
+  // Add fixed header on scroll
+  if (scroll > 0) {
     header.classList.add('header--fixed');
   } else {
     header.classList.remove('header--fixed')
   }
+
+  // Scroll to top button
+  if (scroll > header.offsetHeight) {
+    scrollToTop.classList.add('scroll-top--active');
+  } else {
+    scrollToTop.classList.remove('scroll-top--active');
+  }
 })
+
 
 // Open Mobile Menu on click
 burger.addEventListener('click', function() {
   mobileMenu.classList.toggle('header__mobile--active');
 })
 
-// Close Mobile Menu on click outside
+scrollToTop.addEventListener('click', function(){
+  window.scrollTo({
+    behavior: "smooth",
+    top: 0
+  })
+})
+
+
+
 document.addEventListener('click', function(e) {
+  // Close Mobile Menu on click outside
   if (e.target.closest('.burger') || e.target.closest('.header__mobile')) {
     return
   } else {
     mobileMenu.classList.remove('header__mobile--active');
   }
 
+  // Stop searching on click outside
   if (e.target.closest('#search-input')) {
     return
   } else {
     document.querySelector('#input-close-btn').classList.remove('input-search__close--active');
     header.classList.remove('header--active');
-    document.querySelector('.header__right').style.display = "flex"
+    document.querySelector('.header__right').classList.remove('header__right--closed');
     document.querySelector('.header__nav').classList.remove('header__nav--closed');
     searchInput.style.width = 250 + "px";
     headerInfo.style.paddingTop = '';
@@ -60,10 +82,11 @@ const headerInfo = document.querySelector('#header-info');
 const headerInput = document.querySelector('.header__input');
 let width = orderBtn.offsetHeight - headerInput.offsetHeight;
 
+// Change header on input click
 searchInput.addEventListener('click', function() {
   document.querySelector('#input-close-btn').classList.add('input-search__close--active');
   document.querySelector('.header__nav').classList.add('header__nav--closed');
-  document.querySelector('.header__right').style.display = "none"
+  document.querySelector('.header__right').classList.add('header__right--closed');
   header.classList.add('header--active');
   headerInfo.style.paddingTop = width/2 - 1 + 'px';
   headerInfo.style.paddingBottom = width/2 - 1 + 'px';
